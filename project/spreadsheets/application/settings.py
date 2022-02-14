5a379cca789c5411258382f4562a55c1d718d813
ReAtlaz <68824306+PlayAtlas@users.noreply.github.com>
2022-02-14 10:51:46 +0300

    Update settings.py

--- project/spreadsheets/application/settings.py
+++ project/spreadsheets/application/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--in3k7s!^j+okb)8%5w15qt&w^sf&jtwjto%pa8nea0ssq)(3b'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
